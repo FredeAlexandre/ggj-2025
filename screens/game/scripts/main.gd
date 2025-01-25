c8323a3acb2009a9812956018178309969f7fde5
Alexandre Frede <alexandre.frede.pro@proton.me>
2025-01-25 15:30:51 +0100

    add things

--- screens/game/scripts/main.gd
+++ screens/game/scripts/main.gd
@@ -1,17 +1,17 @@
 extends Node2D
 
-@export var circle_scene: PackedScene  # Charge la scène du cercle depuis l'inspecteur
+@export var bubble_scene: PackedScene  # Charge la scène du cercle depuis l'inspecteur
 
 func _input(event: InputEvent):
 	if (event is InputEventScreenTouch or event is InputEventMouseButton) and event.pressed:
 		# Instancier le cercle
-		var circle_instance = circle_scene.instantiate()
-		add_child(circle_instance)
+		var bubble = bubble_scene.instantiate()
+		add_child(bubble)
 		
 		# Positionner le cercle à l'endroit du toucher
-		circle_instance.position = event.position
+		bubble.position = event.position
 		
 		await get_tree().create_timer(2.0).timeout
 		
-		circle_instance.queue_free()
+		bubble.queue_free()
 		
