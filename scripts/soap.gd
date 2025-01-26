3bce0ab2389aeb8f88f5526e58628cbe26cec348
Alexandre Frede <alexandre.frede.pro@proton.me>
2025-01-26 15:34:39 +0100

    add max speed

--- scripts/soap.gd
+++ scripts/soap.gd
@@ -1,9 +1,13 @@
 extends RigidBody2D
+
+@export var max_speed = 2000.0
+
 func _ready() -> void:
 	print("Soap is ready !")
 	
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	print(state.get_constant_force())
+	if state.linear_velocity.length() > max_speed:
+		state.linear_velocity = state.linear_velocity.normalized() * max_speed
 
 func _on_body_entered(body: Node) -> void:
 	print("On body enter")
